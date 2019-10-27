4ea6904014ca1c8e53b4c96f13acb04e78fdfc44
roxanalupu <roxana.lupu18@gmail.com>
2019-10-27 11:34:24 +0200

    run 3 threads - display a message when all are on the terminated status

--- src/main/java/com/springbootjpa/SpringBootJpa.java
+++ src/main/java/com/springbootjpa/SpringBootJpa.java
@@ -16,6 +16,39 @@ public class SpringBootJpa {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpa.class, args);
 		
+		Runnable runnable1 = () -> { 
+		    for(int i=0; i< 10; i++) {
+		    	System.out.println("-- Thread 1 --");
+		    }
+		};
+		
+		Runnable runnable2 = () -> { 
+		    for(int i=0; i< 10; i++) {
+		    	System.out.println("-- Thread 2 --");
+		    }
+		};
+		
+		Runnable runnable3 = () -> { 
+		    for(int i=0; i< 10; i++) {
+		    	System.out.println("-- Thread 3 --");
+		    }
+		};
+		
+		Thread thread1 = new Thread(runnable1);
+		thread1.start();
+		Thread thread2 = new Thread(runnable2);
+		thread2.start();
+		Thread thread3 = new Thread(runnable3);
+		thread3.start();
+		
+		while(thread1.isAlive() && thread2.isAlive() && thread3.isAlive()) {
+			System.out.println("Thread has not finished");
+	    } 
+	        
+		System.out.println("Finished");
+	    		
+		
+				
 	}
 
 }
